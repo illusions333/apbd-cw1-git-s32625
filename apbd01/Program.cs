3c61b30e981273625ea14d3f421351e25f840dd4
Oleksandr Dubchak <s32625@pjwstk.edu.pl>
2026-03-10 15:18:36 +0100

    Add one more 'Hello, World!' in the loop

--- apbd01/Program.cs
+++ apbd01/Program.cs
@@ -1,29 +1,30 @@
 ﻿for (int i = 0; i < 10; i++)
 {
     Console.WriteLine("Hello, World! " + (i + 1) + " time");
+    Console.WriteLine("Hello, World! Once more!");
 }
-// Console.WriteLine("Enter 2 numbers you want to operate with");
-// int a = Convert.ToInt32(Console.ReadLine());
-// int b = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Enter an operation");
-// char operation = Convert.ToChar(Console.Read());
-// Console.Write("The result is ");
-// if (operation == '+')
-// {
-//     Console.WriteLine(a + b);
-// }
-// else if (operation == '-')
-// {
-//     Console.WriteLine(a - b);
-// }
-// else if (operation == '*')
-// {
-//     Console.WriteLine(a * b);
-// }
-// else if (operation == '/')
-// {
-//     Console.WriteLine((float) a / (float) b);
-// }
+Console.WriteLine("Enter 2 numbers you want to operate with");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter an operation");
+char operation = Convert.ToChar(Console.Read());
+Console.Write("The result is ");
+if (operation == '+')
+{
+    Console.WriteLine(a + b);
+}
+else if (operation == '-')
+{
+    Console.WriteLine(a - b);
+}
+else if (operation == '*')
+{
+    Console.WriteLine(a * b);
+}
+else if (operation == '/')
+{
+    Console.WriteLine((float) a / (float) b);
+}
 
 static int CalculateMax(int[] values)
 {

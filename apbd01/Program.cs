045a51681876a0b4c6042c90ef3a45c3306567c9
Oleksandr Dubchak <s32625@pjwstk.edu.pl>
2026-03-10 14:54:17 +0100

    Add the feature of finding the maximum in the array

--- apbd01/Program.cs
+++ apbd01/Program.cs
@@ -2,25 +2,48 @@
 {
     Console.WriteLine("Hello, World! " + (i + 1) + " time");
 }
-Console.WriteLine("Enter 2 numbers you want to operate with");
-int a = Convert.ToInt32(Console.ReadLine());
-int b = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter an operation");
-char operation = Convert.ToChar(Console.Read());
-Console.Write("The result is ");
-if (operation == '+')
+// Console.WriteLine("Enter 2 numbers you want to operate with");
+// int a = Convert.ToInt32(Console.ReadLine());
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter an operation");
+// char operation = Convert.ToChar(Console.Read());
+// Console.Write("The result is ");
+// if (operation == '+')
+// {
+//     Console.WriteLine(a + b);
+// }
+// else if (operation == '-')
+// {
+//     Console.WriteLine(a - b);
+// }
+// else if (operation == '*')
+// {
+//     Console.WriteLine(a * b);
+// }
+// else if (operation == '/')
+// {
+//     Console.WriteLine((float) a / (float) b);
+// }
+
+static int CalculateMax(int[] values)
 {
-    Console.WriteLine(a + b);
+    int max = values[0];
+    for (int i = 1; i < values.Length; i++)
+    {
+        if (values[i] > max)
+        {
+            max = values[i];
+        }
+    }
+    return max;
 }
-else if (operation == '-')
+
+Console.WriteLine("Enter the amount of numbers you want to find the maximum from");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the numbers");
+List<int> numbers = new List<int>();
+for (int i = 0; i < n; i++)
 {
-    Console.WriteLine(a - b);
+    numbers.Add(Convert.ToInt32(Console.ReadLine()));
 }
-else if (operation == '*')
-{
-    Console.WriteLine(a * b);
-}
-else if (operation == '/')
-{
-    Console.WriteLine((float) a / (float) b);
-}
+Console.WriteLine("The maximum is equal to "  + CalculateMax(numbers.ToArray()));

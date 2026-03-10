4c7e2ee499e979207b725190f81d2a489cb1d711
Oleksandr Dubchak <s32625@pjwstk.edu.pl>
2026-03-10 14:41:22 +0100

    Add a simple calculator

--- apbd01/Program.cs
+++ apbd01/Program.cs
@@ -2,3 +2,25 @@
 {
     Console.WriteLine("Hello, World! " + (i + 1) + " time");
 }
+Console.WriteLine("Enter 2 numbers you want to operate with");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter an operation");
+char operation = Convert.ToChar(Console.Read());
+Console.Write("The result is ");
+if (operation == '+')
+{
+    Console.WriteLine(a + b);
+}
+else if (operation == '-')
+{
+    Console.WriteLine(a - b);
+}
+else if (operation == '*')
+{
+    Console.WriteLine(a * b);
+}
+else if (operation == '/')
+{
+    Console.WriteLine((float) a / (float) b);
+}
